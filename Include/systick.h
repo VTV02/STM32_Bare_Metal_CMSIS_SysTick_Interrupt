8e89cdc489d4ac6773f5aeafa0cafedd6a4ddc93
VTV02 <93756924+VTV02@users.noreply.github.com>
2024-06-18 10:53:24 +0700

    Create systick.h

---
+++ Include/systick.h
@@ -0,0 +1,14 @@
+/*
+ * systick.h
+ *
+ *  Created on: May 30, 2024
+ *      Author: VOVAN
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+void systickDelayMS(int delay);
+void systick_1hz_interrrupt(void);
+
+#endif /* SYSTICK_H_ */
